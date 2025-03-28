2e3b52e7dd4a5fc5b032de90a21d684c03c1d3fb
Yindong Xiao <xydarcher@qq.com>
2025-03-28 11:06:15 +0800

    use parameter to check if port is necessary.

--- lib/src/main/scala/spinal/lib/blackbox/xilinx/ultrascale/IO.scala
+++ lib/src/main/scala/spinal/lib/blackbox/xilinx/ultrascale/IO.scala
@@ -4,12 +4,13 @@ import spinal.core._
 
 /** Enhanced OSERDESE3 with 8-bit data width */
 case class OSERDESE3(
-  dataWidth: Int = 8,
-  init: Boolean = false,
-  isClkInverted: Boolean = false,
-  isRstInverted: Boolean = false,
-  isClkDivInverted: Boolean = false,
-  simDevice: String = "ULTRASCALE"
+   dataWidth: Int = 8,
+   init: Boolean = false,
+   isClkInverted: Boolean = false,
+   isRstInverted: Boolean = false,
+   isClkDivInverted: Boolean = false,
+   simDevice: String = "ULTRASCALE",
+   hasTristate: Boolean = false
 ) extends BlackBox {
   val generic = new Generic {
     val DATA_WIDTH = dataWidth
@@ -25,13 +26,17 @@ case class OSERDESE3(
   val D = in Bits(dataWidth bits)
   val OQ = out Bool()
   val RST = in Bool()
-  val T = in Bool()
-  val T_OUT = out Bool()
+  val T = hasTristate generate in Bool()
+  val T_OUT = hasTristate generate out Bool()
 
   mapCurrentClockDomain(CLK, RST)
 }
 
-/** Enhanced ODELAYE3 with 9-bit delay resolution */
+/**
+ * Enhanced ODELAYE3 with 9-bit delay resolution
+ * @param cascade "NONE" (default), "MASTER" or "SLAVE"
+ *                When "NONE", CASC_IN and CASC_OUT should be left unconnected
+ */
 case class ODELAYE3(
   cascade: String = "NONE",
   delayFormat: String = "TIME",
@@ -55,11 +60,16 @@ case class ODELAYE3(
     val UPDATE_MODE = updateMode
   }
 
-  val CASC_OUT      = out Bool()
+  require(Set("NONE", "MASTER", "SLAVE").contains(cascade), "Invalid cascade mode (must be NONE, MASTER or SLAVE)")
+  
+  // Required ports
   val CNTVALUEOUT   = out UInt(9 bits)
   val DATAOUT       = out Bool()
-  val CASC_IN       = in Bool()
-  val CASC_RETURN   = in Bool()
+  
+  // Conditionally generated cascaded ports
+  val CASC_OUT      = (cascade != "NONE") generate out Bool()
+  val CASC_IN       = (cascade != "NONE") generate in Bool()
+  val CASC_RETURN   = (cascade != "NONE") generate in Bool()
   val CE            = in Bool()
   val CLK           = in Bool()
   val CNTVALUEIN    = in UInt(9 bits)
@@ -94,14 +104,17 @@ case class IDELAYE3(
     val UPDATE_MODE = updateMode
   }
 
-  val CASC_OUT      = out Bool()
-  val CNTVALUEOUT   = out Bits(9 bits)
+  require(Set("NONE", "MASTER", "SLAVE").contains(cascade), "Invalid cascade mode (must be NONE, MASTER or SLAVE)")
+  
+  val CNTVALUEOUT   = out UInt(9 bits)
   val DATAOUT       = out Bool()
-  val CASC_IN       = in Bool()
-  val CASC_RETURN   = in Bool()
+
+  val CASC_OUT      = (cascade != "NONE") generate out Bool()
+  val CASC_IN       = (cascade != "NONE") generate in Bool()
+  val CASC_RETURN   = (cascade != "NONE") generate in Bool()
   val CE            = in Bool()
   val CLK           = in Bool()
-  val CNTVALUEIN    = in Bits(9 bits)
+  val CNTVALUEIN    = in UInt(9 bits)
   val DATAIN        = in Bool()
   val IDATAIN       = in Bool()
   val INC           = in Bool()
@@ -112,16 +125,16 @@ case class IDELAYE3(
 
 /** Enhanced IOBUF_DCIEN with termination control */
 case class IOBUF_DCIEN(
-  simDevice: String = "ULTRASCALE",
-  useIbufDisable: String = "TRUE"
+   simDevice: String = "ULTRASCALE",
+   useIbufDisable: Boolean = true
 ) extends BlackBox {
-  val generic = new Generic {
-    val SIM_DEVICE = simDevice
-    val USE_IBUFDISABLE = useIbufDisable
-  }
+   val generic = new Generic {
+      val SIM_DEVICE = simDevice
+      val USE_IBUFDISABLE = if(useIbufDisable) "TRUE" else "FALSE"
+   }
 
-  val DCITERMDISABLE = in Bool()
-  val IBUFDISABLE    = in Bool()
+   val DCITERMDISABLE = useIbufDisable generate in Bool()
+   val IBUFDISABLE    = useIbufDisable generate in Bool()
   val I              = in Bool()
   val T              = in Bool()
   val O              = out Bool()
@@ -152,34 +165,34 @@ case class IOBUF() extends BlackBox {
 
 /** Updated ISERDESE3 with FIFO support */
 case class ISERDESE3(
-  dataWidth: Int = 8,
-  fifoEnable: String = "FALSE",
-  fifoSyncMode: String = "FALSE",
-  isClkInverted: Boolean = false,
-  isClkBInverted: Boolean = false,
-  isRstInverted: Boolean = false,
-  simDevice: String = "ULTRASCALE_PLUS"
+   dataWidth: Int = 8,
+   fifoEnable: Boolean = false,
+   fifoSyncMode: Boolean = false,
+   isClkInverted: Boolean = false,
+   isClkBInverted: Boolean = false,
+   isRstInverted: Boolean = false,
+   simDevice: String = "ULTRASCALE_PLUS"
 ) extends BlackBox {
-  val generic = new Generic {
-    val DATA_WIDTH = dataWidth
-    val FIFO_ENABLE = fifoEnable
-    val FIFO_SYNC_MODE = fifoSyncMode
-    val IS_CLK_INVERTED = if(isClkInverted) "1" else "0"
-    val IS_CLK_B_INVERTED = if(isClkBInverted) "1" else "0"
-    val IS_RST_INVERTED = if(isRstInverted) "1" else "0"
-    val SIM_DEVICE = simDevice
-  }
+   val generic = new Generic {
+     val DATA_WIDTH = dataWidth
+     val FIFO_ENABLE = if(fifoEnable) "TRUE" else "FALSE"
+     val FIFO_SYNC_MODE = if(fifoSyncMode) "TRUE" else "FALSE"
+     val IS_CLK_INVERTED = if(isClkInverted) "1" else "0"
+     val IS_CLK_B_INVERTED = if(isClkBInverted) "1" else "0"
+     val IS_RST_INVERTED = if(isRstInverted) "1" else "0"
+     val SIM_DEVICE = simDevice
+   }
 
-  val FIFO_EMPTY       = out Bool()
-  val INTERNAL_DIVCLK  = out Bool()
-  val Q                = out Bits(8 bits)
-  val CLK              = in Bool()
-  val CLK_B            = in Bool()
-  val CLKDIV           = in Bool()
-  val D                = in Bool()
-  val FIFO_RD_CLK      = in Bool()
-  val FIFO_RD_EN       = in Bool()
-  val RST              = in Bool()
+   val FIFO_EMPTY       = fifoEnable generate out Bool()
+   val INTERNAL_DIVCLK  = out Bool()
+   val Q                = out Bits(8 bits)
+   val CLK              = in Bool()
+   val CLK_B            = in Bool()
+   val CLKDIV           = in Bool()
+   val D                = in Bool()
+   val FIFO_RD_CLK      = fifoEnable generate in Bool()
+   val FIFO_RD_EN       = fifoEnable generate in Bool()
+   val RST              = in Bool()
 }
 
 /** IDELAYCTRL for UltraScale (enhanced calibration control) */
